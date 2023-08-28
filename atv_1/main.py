bb1e0b6a82125af1f7d3570dd3620e04caf624d0
Luzia-Silva <aluziagabriela@gmail.com>
2023-08-28 15:41:18 -0300

    Create code Radar Inteligente

--- atv_1/main.py
+++ atv_1/main.py
@@ -1,24 +1,53 @@
+from collections import Counter
+import os 
+
 def Calibrate():
     number_cars = int(
         input("Insira a quantidade de carros que deseja no histórico:"))
     vpm = 0
+    total_value = 0
+    total_weight = 0
     informations_velocity = []
     if number_cars > 0:
         for cars_informations in range(number_cars):
-            calculo = int(cars_informations + 1) * 2
-            print(calculo)
             print("\n \nInsira informações do",
                   cars_informations + 1, "carro:")
             velocity = int(
                 input("Insira a velocidade: "))
             accident = int(
                 input("Insira se houve acidente com 1 para SIM ou 0 para NÃO:"))
-            if velocity > 10 and accident in [0, 1]:
-                informations_velocity.append([velocity, accident])
+            if velocity > 10 and accident == 0:
+                informations_velocity.append(velocity) 
+        value_vpm = Counter(informations_velocity)
+        vpm_sum = sum(value * count for value, count in value_vpm.items())
+        total_values = len(informations_velocity)
+        vpm = vpm_sum / total_values
+        return vpm
 
-        print("Velocidade Máxima Permitida: " + str(vpm))
-    if informations_velocity:
-        print(informations_velocity[0][0])
+
+def Velocity_car(vpm):
+    velocity= int(input("Qual a velocidade do veículo?"))
+    print("velocity =", str(velocity), "vpm =", str(vpm))
+
+    if velocity > vpm:
+        print("Veículo Multado!")
+    else:
+        print("Veículo está OK!") 
 
 
-Calibrate()
+print("\n \nDigite 1 para entrar em modo de Calibração, 2 para entrar em modo de execução e  3 para sair")
+while 1: 
+    print("\n \nRadar em Execução")
+    type_opc = int(input("Escolha o modo de operação:"))
+    if type_opc == 1:
+        vpm = Calibrate()
+    elif type_opc == 2:
+        if "vpm" in locals():
+           Velocity_car(vpm)
+        else:
+            print("\nNão podmeos iniciar se não houver uma base com dados sobre a Velocidade Máxima Permitada!")
+            print("Retorne e faça o abastecimento da base VPM!")
+    else: 
+        break
+
+        
